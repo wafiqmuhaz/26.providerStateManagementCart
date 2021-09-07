e5eefe3a96299d9b2e08ffe304a81798768b44d1
Wafiq Muhammad Abdul Aziz <44719192+wafiqmuhaz@users.noreply.github.com>
2021-09-07 17:48:40 +0700

    Add files via upload

---
+++ 26.providerStateManagementCart.dart
@@ -0,0 +1,121 @@
+import 'package:eriko/money.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'cart.dart';
+
+void main() {
+  runApp(app());
+}
+
+// ignore: camel_case_types
+class app extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider<Money>(
+            create: (context) => Money(),
+          ),
+          ChangeNotifierProvider<Cart>(
+            create: (context) => Cart(),
+          ),
+        ],
+        child: Scaffold(
+          floatingActionButton: Consumer(
+            builder: (context, money, _) => Consumer<Cart>(
+              builder: (context, cart, _) => FloatingActionButton(
+                onPressed: () {
+                  if (money.balance >= 500) {
+                    // ignore: unnecessary_statements
+                    cart.quantity + -1;
+                    money.balance -= 500;
+                  }
+                },
+                child: Icon(Icons.add_shopping_cart),
+                backgroundColor: Colors.purple,
+              ),
+            ),
+          ),
+          appBar: AppBar(
+            backgroundColor: Colors.purple,
+            title: Text("Multi Provider"),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Saldo Anda"),
+                    Container(
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Consumer<Money>(
+                          builder: (context, money, _) => Text(
+                            money.balance.toString(),
+                            style: TextStyle(
+                                color: Colors.purple,
+                                fontWeight: FontWeight.w700),
+                          ),
+                        ),
+                      ),
+                      height: 30,
+                      width: 150,
+                      margin: EdgeInsets.all(5),
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: Colors.purple[100],
+                        border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Consumer<Cart>(
+                      builder: (context, cart, _) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            "Mangga (20000) x " + cart.quantity.toString(),
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            (5000 * cart.quantity).toString(),
+                            style: TextStyle(
+                                color: Colors.black45,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  height: 30,
+                  margin: EdgeInsets.all(5),
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 2,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
